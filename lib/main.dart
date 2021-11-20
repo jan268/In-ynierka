a2adcd2d0f873101ec8de3f1842501bd87caf260
Jan <jan.witek88@gmail.com>
2021-11-20 17:24:38 +0100

    added filter page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:net_market/objects/item_card_object.dart';
 import 'package:net_market/pages/buy_item.dart';
+import 'package:net_market/pages/filters.dart';
 import 'package:net_market/pages/home.dart';
 import 'package:net_market/pages/item_card.dart';
 import 'package:net_market/pages/loading.dart';
@@ -10,7 +11,7 @@ import 'package:net_market/pages/search.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: "/search",
+    initialRoute: "/filters",
     routes: {
       '/' : (context) => Loading(),
       '/home' : (context) => Home(category: 'Streetwear',),
@@ -18,6 +19,7 @@ void main() {
       '/buy_item' : (context) => BuyItem(itemCardObject: ItemCardObject(), size: '',),
       '/item_card' : (context) => ItemCard(id: '',),
       '/search' : (context) => SearchPage(category: 'Streetwear',),
+      '/filters' : (context) => FilterPage(),
     },
   ));
 }
--- lib/pages/filters.dart
+++ lib/pages/filters.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FilterPage extends StatefulWidget {
@@ -8,8 +9,206 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
+
+  final myController = TextEditingController();
+  final myController2 = TextEditingController();
+  final myController3 = TextEditingController();
+  String dropdownValue = "All";
+  String priceValue = "All prices";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SafeArea(
+          child:  Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Name:",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: TextField(
+                      controller: myController,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Make:",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: TextField(
+                      controller: myController2,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Model:",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: TextField(
+                      controller: myController3,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Gender:",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.transgender),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['All', 'Men', 'Women', 'Kids', 'Unisex']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    )
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Price:",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      child: DropdownButton<String>(
+                        value: priceValue,
+                        icon: const Icon(CupertinoIcons.money_dollar),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: const TextStyle(color: Colors.deepPurple),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            priceValue = newValue!;
+                          });
+                        },
+                        items: <String>['All prices', 'Under 100', '100-200', '200-300', '300-400', '400-500', '500+']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      )
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width/2,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              ),
+              onPressed: () {
+                // powrot do ekranu wyszukiwania z (przekazac dane filtrow)
+              },
+              child: Text("APPLY"),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width/2,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              ),
+              onPressed: () {
+                // powrot do ekranu wyszukiwania z (przekazac dane filtrow)
+              },
+              child: Text("CLEAR"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
